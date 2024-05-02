3046aa24434f382b1ce2941a2af64e68ce86a95c
reza miraki <reza.miraki10@gmail.com>
2024-05-02 20:09:08 +0330

    [fix]: refactor parse method in spider

--- devto/devto/spiders/devto_spider.py
+++ devto/devto/spiders/devto_spider.py
@@ -19,38 +19,36 @@ class DevtoSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'https://dev.to/glitch/what-is-worth-learning-41e3',
+            'https://dev.to/lundjrl/reflecting-on-my-programming-career-51b6',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
 
-        if response.status == 200:
-
-            doc_id = response.css(self.css_selectors['doc_id']).get()
-            if not is_exist(doc_id):
-                try:
-                    title = response.css(self.css_selectors['title']).get().strip()
-                    author = response.css(self.css_selectors['author']).get().strip()
-                    tags = response.css(self.css_selectors['tags']).getall()
-                    publish_date = response.css(self.css_selectors['publish_date']).get().split('T')[0]
-
-                    save_doc_id(doc_id=doc_id)
-                    save_doc_body(response, self.css_selectors['article_body'], doc_id=doc_id, title=title,
-                                  author=author,
-                                  tags=tags, publish_date=publish_date)
-
-                except Exception as e:
-                    self.logger.error(e)
-
-                next_pages = response.css(self.css_selectors['read_next']).getall()
-                if self.current_depth < self.max_depth:
-                    self.current_depth += 1
-                    for next_page in next_pages:
-                        yield response.follow(next_page, callback=self.parse)
-
-            else:
-                self.logger.info(f"Document with {doc_id} already exists. Skipping.")
-        else:
+        if response.status != 200:
             self.logger.error(f"Failed to fetch page: {response.url}")
+            return
+
+        doc_id = response.css(self.css_selectors['doc_id']).get()
+        if is_exist(doc_id):
+            self.logger.info(f"Document with {doc_id} already exists. Skipping.")
+
+        try:
+            title = response.css(self.css_selectors['title']).get().strip()
+            author = response.css(self.css_selectors['author']).get().strip()
+            tags = response.css(self.css_selectors['tags']).getall()
+            publish_date = response.css(self.css_selectors['publish_date']).get().split('T')[0]
+
+            save_doc_id(doc_id=doc_id)
+            save_doc_body(response, self.css_selectors['article_body'], doc_id=doc_id, title=title, author=author,
+                          tags=tags, publish_date=publish_date)
+
+        except Exception as e:
+            self.logger.error(e)
+
+        next_pages = response.css(self.css_selectors['read_next']).getall()
+        if self.current_depth < self.max_depth:
+            self.current_depth += 1
+            for next_page in next_pages:
+                yield response.follow(next_page, callback=self.parse)
